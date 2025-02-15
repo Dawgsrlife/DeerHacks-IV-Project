ed555a07b23882898262b08d841f5d837ea31f31
mengale1 <alex.meng@mail.utoronto.ca>
2025-02-15 18:40:34 -0500

    Configs and app hookup

---
+++ frontend/src/routes.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// Admin Imports
+import MainDashboard from "views/admin/default";
+import Profile from "views/admin/profile";
+
+// Icon Imports
+import { MdHome, MdPerson } from "react-icons/md";
+
+const routes = [
+  {
+    name: "Main Dashboard",
+    layout: "/admin",
+    path: "default",
+    icon: <MdHome className="h-6 w-6" />,
+    component: <MainDashboard />,
+  },
+  {
+    name: "Profile",
+    layout: "/admin",
+    path: "profile",
+    icon: <MdPerson className="h-6 w-6" />,
+    component: <Profile />,
+  },
+];
+
+export default routes;
