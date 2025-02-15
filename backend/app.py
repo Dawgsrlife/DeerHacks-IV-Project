5adec30d3dcb11133845c0e995c5fdb46b9858a6
huaethan <ethanhua7378@gmail.com>
2025-02-15 16:34:06 -0500

    upload_with_tag complete

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
 import os
 import types
+import io
+
+import requests
 
 from flask import Flask, request, jsonify
 from PIL import Image
-from typing import Union,Sequence
+from typing import Union, Sequence, BinaryIO, Any
 
 from flask.cli import load_dotenv
 from google.cloud import vision
@@ -31,9 +34,10 @@ from openai import OpenAI
 # import io
 # import os
 
+
 app = Flask(__name__)
 
-context = "You are the brain of a system with a tagged photo gallery and a to-do list. Below are the existing tags:"
+prmt_context = "You are the brain of a system with a tagged photo gallery and a to-do list. Below are the existing tags:"
 instruction = ("The user will provide you with a description of what they are trying to find, which can range from a specific photo or a reminder on the to-do list." +
              "Your job is to figure out which tags are related to their description and provide it to them. The only thing you should respond with are the tags you deemed to be fitting, each on a new line, or ERROR if the user input does not make sense.\n" +
              "For example, suppose the system contains the following tags:\nschool notes\nscenery\nfood\nmath\n\nSuppose the user asks \"Find the math problem I was working on last week.\" Below is what your response should be:\n"
@@ -42,7 +46,7 @@ instruction = ("The user will provide you with a description of what they are tr
 # list of tags
 tags = []
 
-sys_instr = (context + "\nSTART OF TAGS\n" +
+sys_instr = (prmt_context + "\nSTART OF TAGS\n" +
             "\n".join(tags) + "\nEND OF TAGS\n" +
              instruction)
 
@@ -81,22 +85,29 @@ def upload():
     # categorize(context) ...
     # get file from front end
     req_file = request.files.get("image")
-    # verify file integrity
-    verified_file = load_image(req_file)
-    if verified_file is None:
-        # do error
-        pass
+    path = request.args.get("path")
+    name = request.args.get("name")
+    format = request.args.get("format")
+    img_str = path + name + '.' + format
+    # # verify file integrity
+    # verified_file = load_image(req_file)
+    # if verified_file is None:
+    #     return jsonify({'error': 'invalid '}), 400
+    #     pass
     # get image context from front end
     # req_context = str(request.args.get('context')) # url / input most have context=... and his gets ...
     # get tags from deepseek (issue: deepseek might not return same tags for same input)
-    unprocessed_tags = categorize(verified_file)
+    unprocessed_tags = categorize(req_file)
+    print(unprocessed_tags)
     processed_tags = process_ai_response(unprocessed_tags)
-
+    assign_tags_to_image(processed_tags, img_str)
+    print(tags)
+    return jsonify({"tags": processed_tags})
 
 '''
-given a context string, have DeepSeek categorize the text into several tags, both new and existing
+given a context string, categorize the text into several tags, both new and existing
 '''
-def categorize(context: Image.Image) -> list[str]:
+def categorize(context: BinaryIO) -> Any:
 
     # change api key as needed
     auth = authenticate_with_api_key(os.getenv("GOOGLE_VISION_KEY"))
@@ -104,9 +115,10 @@ def categorize(context: Image.Image) -> list[str]:
         print("Authentication failed")
         return []
     client = vision.ImageAnnotatorClient()
-
-    img = vision.Image()
-    img.source.image_uri = img.image_uri(context)
+    # image_file = io.BytesIO()
+    # context.save(image_file, format=context.format)
+    img = vision.Image(content=context.read())
+    # img.source.image_uri = img.image_uri(context)
 
     fts = [
         vision.Feature.Type.LABEL_DETECTION,
@@ -126,28 +138,28 @@ def process_ai_response(response) -> list[str]:
     # label processing
     for label in response.label_annotations:
         guarantee = label.score
-        tag = label.description
+        tag = label.description.lower()
 
         # add condition for score
-        if tag not in tags:
+        if tag not in img_tags:
             img_tags.append(tag)
 
     for landmark in response.landmark_annotations:
         guarantee = landmark.score
-        tag = landmark.description
-        if tag not in images_to_tags:
+        tag = landmark.description.lower()
+        if tag not in img_tags:
             img_tags.append(tag)
 
     for obj in response.localized_object_annotations:
         guarantee = obj.score
-        tag = obj.description
-        if tag not in tags_to_image:
+        tag = obj.name.lower()
+        if tag not in img_tags:
             img_tags.append(tag)
 
     for logo in response.logo_annotations:
         guarantee = logo.score
-        tag = logo.description
-        if tag not in images_to_tags:
+        tag = logo.description.lower()
+        if tag not in img_tags:
             img_tags.append(tag)
 
     return img_tags
@@ -156,7 +168,7 @@ def process_ai_response(response) -> list[str]:
 def assign_tags_to_image(input_tags: list[str], image):
 
     # Assign tags to images
-    images_to_tags.update(image, input_tags)
+    images_to_tags[image] = input_tags
 
     # Assign image to tags
     for tag in input_tags:
@@ -164,30 +176,32 @@ def assign_tags_to_image(input_tags: list[str], image):
 
 def update_system_instructions():
     global sys_instr
-    sys_instr = (context + "\nSTART OF TAGS\n" +
+    sys_instr = (prmt_context + "\nSTART OF TAGS\n" +
             "\n".join(tags) + "\nEND OF TAGS\n" +
              instruction)
 
-'''
-Testing function
-'''
-def foo():
-    tags.append('games')
-    tags.append('homework')
-    tags_to_image['games'] = ['league of legends', 'overwatch']
-    images_to_tags['homework'] = ['csc236']
+# '''
+# Testing function
+# '''
+# def foo():
+#     tags.append('games')
+#     tags.append('homework')
+#     tags_to_image['games'] = ['league of legends', 'overwatch']
+#     images_to_tags['homework'] = ['csc236']
 
 
 #==============================================================================
 
 
 '''
-Given a context string describing image(s) to be found, have DeepSeek compile a list of existing tags that match the context
+Given a context string describing image(s) to be found, compile a list of existing tags that match the context
 '''
 @app.route('/search', methods=['GET'])
 def search():
     description = request.args.get('desc')
     suitable_tags = get_tags(description)
+    if not suitable_tags:
+        return jsonify({'error': 'No tags found'}), 404
 
 
 def get_tags(description: str) -> list[str]:
