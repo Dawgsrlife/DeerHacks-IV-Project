4cd5f5a58a250436c37420f1eb75e9be2d34f6c0
mengale1 <alex.meng@mail.utoronto.ca>
2025-02-15 20:21:16 -0500

    Re-order routes and rename dashboard to statistics

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,26 +1,26 @@
 import React from "react";
 
 // Admin Imports
-import MainDashboard from "views/admin/default";
 import Profile from "views/admin/profile";
+import Statistics from "views/admin/default";
 
 // Icon Imports
-import { MdHome, MdPerson } from "react-icons/md";
+import { MdHome, MdPerson, MdBarChart } from "react-icons/md"; // Added MdBarChart for Statistics
 
 const routes = [
-  {
-    name: "Main Dashboard",
-    layout: "/admin",
-    path: "default",
-    icon: <MdHome className="h-6 w-6" />,
-    component: <MainDashboard />,
-  },
   {
     name: "Profile",
     layout: "/admin",
     path: "profile",
     icon: <MdPerson className="h-6 w-6" />,
     component: <Profile />,
+  },
+  {
+    name: "Statistics",
+    layout: "/admin",
+    path: "default",
+    icon: <MdBarChart className="h-6 w-6" />, // Updated icon to represent stats
+    component: <Statistics />,
   },
 ];
 
