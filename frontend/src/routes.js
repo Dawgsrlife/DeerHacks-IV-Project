56aab6302d76fff5d7fe052fef2a779b9985c0fe
mengale1 <alex.meng@mail.utoronto.ca>
2025-02-16 00:08:13 -0500

    Add the memory page to be part of the view options; improve search bar UI and add navigation to memory results.

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,7 +5,10 @@ import Profile from "views/admin/profile";
 import Statistics from "views/admin/default";
 
 // Icon Imports
-import { MdPerson, MdBarChart } from "react-icons/md"; // Added MdBarChart for Statistics
+import { MdPerson, MdBarChart } from "react-icons/md";
+
+// Memory Page Import
+import MemoryPage from "./views/MemoryPage";
 
 const routes = [
   {
@@ -21,6 +24,12 @@ const routes = [
     path: "statistics",
     icon: <MdBarChart className="h-6 w-6" />, // Updated icon to represent stats
     component: <Statistics />,
+  },
+  {
+    path: "/memory/:query",
+    name: "Memory",
+    layout: "/admin",
+    component: <MemoryPage />,
   },
 ];
 
